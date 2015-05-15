6b49f95f872bac597996fc01b5d15e67b86ec133
sebastienadam <sebastien.adam.webdev@gmail.com>
2015-05-15 15:14:21 +0200

    Query Magasin
    Création des requêtes pour la création de la base de données du magasin.
    Quelques petites modification du schéma E-A.

---
+++ SQLQuery.sql
@@ -0,0 +1,137 @@
+-- Création de la base de données
+create database CampingParadis;
+go
+
+use CampingParadis;
+go
+
+-- création des données du magasin
+create schema Magasin;
+go
+
+create table Magasin.Magasin (
+  ID int identity(1,1) primary key,
+  Nom varchar(64) not null unique,
+  Telephone varchar(32)
+)
+
+create table Magasin.Stock(
+  IDMagasin int not null,
+  IDProduit int not null,
+  Quantite int not null,
+  primary key (IDMagasin, IDProduit)
+)
+
+create table Magasin.Produit(
+  ID int identity(1,1) primary key,
+  Marque varchar(64) not null,
+  PrixUnitaire money not null,
+  Contenance decimal(8,2) not null,
+  Unite varchar(16) not null,
+  IDGamme int not null
+)
+
+create table Magasin.Gamme (
+  ID int identity(1,1) primary key,
+  Libelle varchar(64) not null unique,
+  IDFamille int not null,
+);
+
+create table Magasin.Famille (
+  ID int identity(1,1) primary key,
+  Libelle varchar(64) not null unique,
+);
+
+create table Magasin.FamilleHerarchie (
+  IDFamille int,
+  IDParent int,
+  primary key (IDFamille, IDParent)
+)
+
+insert into Magasin.Magasin (Nom)
+values ('La superette des Bellettes grises');
+
+insert into Magasin.Famille (Libelle)
+values ('Légumes');
+insert into Magasin.Famille (Libelle)
+values ('Conserves');
+insert into Magasin.Famille (Libelle)
+values ('Légumes frais');
+insert into Magasin.Famille (Libelle)
+values ('Légumes surgelés');
+insert into Magasin.Famille (Libelle)
+values ('Conserves de légumes');
+
+insert into Magasin.FamilleHerarchie (IDFamille,IDParent)
+values((select ID from Magasin.Famille where Libelle = 'Légumes frais'),
+       (select ID from Magasin.Famille where Libelle = 'Légumes'));
+insert into Magasin.FamilleHerarchie (IDFamille,IDParent)
+values((select ID from Magasin.Famille where Libelle = 'Légumes surgelés'),
+       (select ID from Magasin.Famille where Libelle = 'Légumes'));
+insert into Magasin.FamilleHerarchie (IDFamille,IDParent)
+values((select ID from Magasin.Famille where Libelle = 'Conserves de légumes'),
+       (select ID from Magasin.Famille where Libelle = 'Légumes'));
+insert into Magasin.FamilleHerarchie (IDFamille,IDParent)
+values((select ID from Magasin.Famille where Libelle = 'Conserves de légumes'),
+       (select ID from Magasin.Famille where Libelle = 'Conserves'));
+
+insert into Magasin.Gamme(Libelle, IDFamille)
+values('Petits pois en conserve',
+       (select ID from Magasin.Famille where Libelle = 'Conserves de légumes'));
+
+insert into Magasin.Produit(Marque, PrixUnitaire, Contenance, Unite, IDGamme)
+values('Marie',1.25,800,'gr',(select ID from Magasin.Gamme where Libelle = 'Petits pois en conserve'));
+insert into Magasin.Produit(Marque, PrixUnitaire, Contenance, Unite, IDGamme)
+values('Marie',0.75,400,'gr',(select ID from Magasin.Gamme where Libelle = 'Petits pois en conserve'));
+insert into Magasin.Produit(Marque, PrixUnitaire, Contenance, Unite, IDGamme)
+values('René',0.49,800,'gr',(select ID from Magasin.Gamme where Libelle = 'Petits pois en conserve'));
+
+insert into Magasin.Stock (IDMagasin, IDProduit, Quantite)
+values((select ID from Magasin.Magasin where Nom = 'La superette des Bellettes grises'),
+       1,
+       25);
+insert into Magasin.Stock (IDMagasin, IDProduit, Quantite)
+values((select ID from Magasin.Magasin where Nom = 'La superette des Bellettes grises'),
+       2,
+       12);
+insert into Magasin.Stock (IDMagasin, IDProduit, Quantite)
+values((select ID from Magasin.Magasin where Nom = 'La superette des Bellettes grises'),
+       3,
+       5);
+
+alter table Magasin.FamilleHerarchie
+add constraint fk_famillehierarchie_familleid
+foreign key (IDFamille) references Magasin.Famille (ID);
+
+alter table Magasin.FamilleHerarchie
+add constraint fk_famillehierarchie_familleparent
+foreign key (IDParent) references Magasin.Famille (ID);
+
+alter table Magasin.Gamme
+add constraint fk_gamme_famille
+foreign key (IDFamille) references Magasin.Famille (ID);
+
+alter table Magasin.Produit
+add constraint fk_produit_gamme
+foreign key (IDGamme) references Magasin.Gamme (ID);
+
+alter table Magasin.Stock
+add constraint fk_stock_produit
+foreign key (IDProduit) references Magasin.Produit (ID);
+
+alter table Magasin.Stock
+add constraint fk_stock_magasin
+foreign key (IDMagasin) references Magasin.Magasin (ID);
+go
+
+create view Magasin.FamilleDetails(ID, Libelle, Parent)
+as
+    select F.ID, F.Libelle, P.Libelle Parent
+    from Magasin.Famille F,Magasin.FamilleHerarchie H, Magasin.Famille P
+    where F.ID = H.IDFamille
+      and H.IDParent = P.ID
+  union
+    select *, null Parent
+    from Magasin.Famille F
+    where ID not in (select distinct IDFamille from Magasin.FamilleHerarchie);
+go
