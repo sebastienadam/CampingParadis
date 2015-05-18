335a974b8b29d8ff4be98d3790bd5473b813ca40
sebastienadam <sebastien.adam.webdev@gmail.com>
2015-05-18 22:54:17 +0200

    Requêtes magasin
    * ajout d'une table pour l'historique des ventes
    * Ajout d'un champ dans la table Stock pour déterminer l'état du stock.
    * création d'un trigger pour empêcher la modification de l'historique de
    vente
    * création d'un trigger pour mettre à jour l'état du stock

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -9,18 +9,36 @@ go
 create schema Magasin;
 go
 
+-- TODO:
+-- * procédure 'vendre produit'
+-- * procédure 'approvisionnement stock'
+-- * procédure 'nouveau produit'
+-- * procédure 'nouvelle gamme'
+-- * procédure 'nouvelle famille' => paramètre 'parent' optionnel
+-- * procédure 'nouvelle hiérarchie famille'
+
 create table Magasin.Magasin (
   ID int identity(1,1) primary key,
   Nom varchar(64) not null unique,
   Telephone varchar(32)
-)
+);
 
 create table Magasin.Stock(
   IDMagasin int not null,
   IDProduit int not null,
   Quantite int not null,
+  Epuise tinyint not null default 0,
   primary key (IDMagasin, IDProduit)
-)
+);
+
+create table Magasin.HistoriqueVente (
+  ID int identity(1,1) primary key,
+  IDProduit int,
+  Prix money,
+  Quantite int,
+  DateVente DateTime,
+  Vendeur varchar(65)
+);
 
 create table Magasin.Produit(
   ID int identity(1,1) primary key,
@@ -29,7 +47,7 @@ create table Magasin.Produit(
   Contenance decimal(8,2) not null,
   Unite varchar(16) not null,
   IDGamme int not null
-)
+);
 
 create table Magasin.Gamme (
   ID int identity(1,1) primary key,
@@ -46,8 +64,44 @@ create table Magasin.FamilleHerarchie (
   IDFamille int,
   IDParent int,
   primary key (IDFamille, IDParent)
-)
+);
+go
 
+create trigger Magasin.trigger_stock_epuise
+on Magasin.Stock
+after insert, update
+as
+begin
+  if update(Quantite) begin
+    declare @IDMag int;
+    declare @IDProd int;
+    declare @Quantite int;
+    declare @Epuise int;
+    select @IDMag = IDMagasin, @IDProd = IDProduit, @Quantite = Quantite
+    from inserted;
+    if(@Quantite = 0) begin
+      set @Epuise = 0;
+    end else if(@Quantite < 5) begin
+      set @Epuise = 1;
+    end else begin
+      set @Epuise = 2;
+    end
+    update Magasin.Stock
+    set Epuise = @Epuise
+    where IDMagasin = @IDMag and IDProduit = @IDProd;
+  end
+end
+go
+
+create trigger Magasin.trigger_historique_vente_non_modifiable
+on Magasin.HistoriqueVente
+after update, delete
+as begin
+  rollback
+end
+go
+
+-- remplissage des tables du magasin
 insert into Magasin.Magasin (Nom)
 values ('La superette des Bellettes grises');
 
@@ -99,6 +153,7 @@ values((select ID from Magasin.Magasin where Nom = 'La superette des Bellettes g
        3,
        5);
 
+-- ajout des contraintes sur les tables du magasin
 alter table Magasin.FamilleHerarchie
 add constraint fk_famillehierarchie_familleid
 foreign key (IDFamille) references Magasin.Famille (ID);
@@ -122,8 +177,13 @@ foreign key (IDProduit) references Magasin.Produit (ID);
 alter table Magasin.Stock
 add constraint fk_stock_magasin
 foreign key (IDMagasin) references Magasin.Magasin (ID);
+
+alter table Magasin.HistoriqueVente
+add constraint fk_historiquevente_produit
+foreign key (IDProduit) references Magasin.Produit (ID);
 go
 
+-- création d'une vue pour afficher les familles avec leur parent
 create view Magasin.FamilleDetails(ID, Libelle, Parent)
 as
     select F.ID, F.Libelle, P.Libelle Parent
